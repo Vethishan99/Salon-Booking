6ac450cb945345f11cae54a2659c603d5c3d0b9b
Vethishan <vethishan96@gmail.com>
2025-09-30 09:58:47 +0530

    Adding user microservice

---
+++ User-Service/src/test/java/com/vethishan/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vethishan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
